d4ddf2f06c586560cd9cf225918be611ccec71ca
Renato Spakauskas <renatospaka@gmail.com>
2021-04-30 18:10:42 -0300

    feat: :construction: person entity domain AB#113

--- emr/domain/person_test.go
+++ emr/domain/person_test.go
@@ -13,46 +13,69 @@ func TestPerson_IsValid(t *testing.T) {
 	person.Name = "Renato"
 	person.LastName = "Spakauskas"
 	person.Email = "re@renato"
-	person.DOB = time.Now()
+	//person.DOB = time.Now().AddDate(-5, 3, -12)
+	person.DOB = time.Date(1970, 11, 14, 0, 0, 0, 0, time.UTC)
 
 	//all working
 	err := person.IsValid()
 	require.Nil(t, err)
 
-	//missing name
 	person.Name = ""
 	err = person.IsValid()
-	require.EqualError(t, err, domain.ERROR_NAME_EMPTY)
-	
-	//missing lastname
+	require.EqualError(t, err, domain.ERROR_NAME_MISSING)
+
+	person.Name = "Re"
+	err = person.IsValid()
+	require.EqualError(t, err, domain.ERROR_NAME_TOO_SHORT)
+
+	person.Name = "RenatoRenatoRenatoRenatoRenatoRenatoRenatoRenatoRenato"
+	err = person.IsValid()
+	require.EqualError(t, err, domain.ERROR_NAME_TOO_LONG)
+
 	person.Name = "Renato"
 	person.LastName = ""
 	err = person.IsValid()
-	require.EqualError(t, err, domain.ERROR_LAST_NAME_EMPTY)
+	require.EqualError(t, err, domain.ERROR_LAST_NAME_MISSING)
 
-	//missing email
+	person.LastName = "Sp"
+	err = person.IsValid()
+	require.EqualError(t, err, domain.ERROR_LAST_NAME_TOO_SHORT)
+
+	person.LastName = "SpakauskasSpakauskasSpakauskasSpakauskasSpakauskasSpakauskas"
+	err = person.IsValid()
+	require.EqualError(t, err, domain.ERROR_LAST_NAME_TOO_LONG)
+
 	person.LastName = "Spakauskas"
 	person.Email = ""
 	err = person.IsValid()
-	require.EqualError(t, err, domain.ERROR_EMAIL_EMPTY)
+	require.EqualError(t, err, domain.ERROR_EMAIL_MISSING)
 
-	//invalid email
 	person.Email = "email"
 	err = person.IsValid()
 	require.EqualError(t, err, domain.ERROR_EMAIL_INVALID)
 
-	//invalid email
 	person.Email = "email@"
 	err = person.IsValid()
 	require.EqualError(t, err, domain.ERROR_EMAIL_INVALID)
 
-	//invalid email
 	person.Email = "em@a"
 	err = person.IsValid()
 	require.EqualError(t, err, domain.ERROR_EMAIL_INVALID)
 
-	//valid email
 	person.Email = "renato@renato"
+	person.DOB = time.Time{}
+	err = person.IsValid()
+	require.EqualError(t, err, domain.ERROR_DOB_MISSING)
+
+	person.DOB = time.Now()
+	err = person.IsValid()
+	require.EqualError(t, err, domain.ERROR_DOB_INVALID)
+
+	person.DOB = time.Now().AddDate(0, 0, 1)		//amanhã
+	err = person.IsValid()
+	require.EqualError(t, err, domain.ERROR_DOB_INVALID)
+
+	person.DOB = time.Now().AddDate(-5, 3, -12)	//5 anos antes
 	err = person.IsValid()
 	require.Nil(t, err)
 }
--- emr/main.go
+++ emr/main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-	"time"
-
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
 	log.Info("Iniciou")
-	
-	var d time.Time
-	log.Info(d.IsZero())
-	
-	d = time.Now()
-	log.Info(d.IsZero())
 }
